680a2bedfc82db554d9c5ffd640ee48edbfde35c
Zeynabrostamy96 <73117940+Zeynabrostamy96@users.noreply.github.com>
2021-08-07 11:50:58 +0430

    viewcomponent in web

---
+++ TopLearn.Web/ViewComponents/CourseGroupComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TapLearn.Core.Services.interfaces;
+
+namespace TopLearn.Web.ViewComponents
+{
+    public class CourseGroupComponent: ViewComponent
+    {
+        private ICourseGroupServices _courseGroupServices;
+        public CourseGroupComponent(ICourseGroupServices courseGroupServices)
+        {
+            _courseGroupServices = courseGroupServices;
+        }    
+        public async  Task<IViewComponentResult> InvokeAsync()
+        {
+            return  await Task.FromResult((IViewComponentResult)View("CourseGroup",_courseGroupServices.GettGroup()));
+        }
+    }
+}
